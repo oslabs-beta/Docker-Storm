8c1419a3d67ee4800efd94d1183b4c353d4d2a3f
Kelvin Van <kelvinvan.cs@gmail.com>
2022-11-28 08:22:27 -0800

    Added some functionality to display a line graph type
    Co-authored-by: Kelvin Van <90666201+KelvinVan1@users.noreply.github.com>
    Co-authored-by: Jeffrey Lee <92632725+jclee8888@users.noreply.github.com>
    Co-authored-by: Shay Sheller <24629189+shaysheller@users.noreply.github.com>
    Co-authored-by: Kevin Tseng <107890711+Kevin-J-Tseng@users.noreply.github.com>

--- src/client/pages/metrics.tsx
+++ src/client/pages/metrics.tsx
@@ -12,8 +12,9 @@ const Metrics = ({ApiKey}) => {
     <div>
       <div>Metrics</div>
       <iframe src={`http://localhost:3000/d-solo/${ApiKey}/test?orgId=1&refresh=5s&panelId=0`} width="450" height="200" frameBorder="0"></iframe>
-      <iframe src={`http://localhost:3000/d-solo/${ApiKey}/test?orgId=1&refresh=5s&panelId=1`} width="450" height="200" frameBorder="0"></iframe>
+      <iframe src={`http://localhost:3000/d-solo/${ApiKey}/test?orgId=1&refresh=5s&panelId=1`} width="900" height="400" frameBorder="0"></iframe>
       <iframe src={`http://localhost:3000/d-solo/${ApiKey}/test?orgId=1&refresh=5s&panelId=2`} width="450" height="200" frameBorder="0"></iframe>
+      <iframe src={`http://localhost:3000/d-solo/${ApiKey}/test?orgId=1&refresh=5s&panelId=3`} width="450" height="200" frameBorder="0"></iframe>
     </div>
 
 
