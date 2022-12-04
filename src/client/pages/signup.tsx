df2fb71ca8a80c5f4efe48d7c78c2ceb6d47db54
jclee8888 <jeffreyclee@gmail.com>
2022-12-04 12:35:51 -0800

    pseudo code clarifications  Co-authored-by: Kelvin Van <90666201+KelvinVan1@users.noreply.github.com> Co-authored-by: Jeffrey Lee <92632725+jclee8888@users.noreply.github.com> Co-authored-by: Shay Sheller <24629189+shaysheller@users.noreply.github.com> Co-authored-by: Kevin Tseng <107890711+Kevin-J-Tseng@users.noreply.github.com>

---
+++ src/client/pages/signup.tsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { DefaultDeserializer } from 'v8';
+
+
+// signup router already exists
+// use a POST request to '/signup'
+
+
+
+// make sure the username does not already exist
+
+// Fields that we need:
+// First Name
+// Last Name
+// Organization
+// Job Title
+// 
